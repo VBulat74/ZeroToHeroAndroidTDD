be55439604e25aa34f36cb9c1733f6449a6890af
VBulat74 <Vladimir.Bulat@gmail.com>
2024-02-04 11:46:34 +0300

    task / 014 solved (coroutine)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,41 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progress = findViewById<ProgressBar>(R.id.progressBar)
+        val btn = findViewById<Button>(R.id.actionButton)
+        val textVierw = findViewById<TextView>(R.id.titleTextView)
+
+        val scope = CoroutineScope(Dispatchers.Main)
+
+        btn.setOnClickListener {
+            btn.isEnabled = false
+            progress.visibility = View.VISIBLE
+
+            scope.launch {
+                delay(3000)
+
+                textVierw.visibility = View.VISIBLE
+
+                progress.visibility = View.GONE
+                btn.isEnabled = true
+
+            }
+
+
+        }
     }
 }
